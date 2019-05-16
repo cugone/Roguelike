2b216a8a86fe108016832524dc832a75dded0f9b
cugone <cugone@gmail.com>
2019-05-16 14:20:01 -0500

    Implemented LoadFromXml

--- Roguelike/Code/Game/Map.cpp
+++ Roguelike/Code/Game/Map.cpp
@@ -1,11 +1,19 @@
 #include "Game/Map.hpp"
 
 #include "Engine/Core/BuildConfig.hpp"
+#include "Engine/Core/DataUtils.hpp"
 #include "Engine/Core/ErrorWarningAssert.hpp"
+#include "Engine/Core/FileUtils.hpp"
 #include "Engine/Renderer/Material.hpp"
 #include "Engine/Renderer/Renderer.hpp"
 
+#include "Game/GameCommon.hpp"
+#include "Game/TileDefinition.hpp"
 #include "Game/Layer.hpp"
+
+#include <sstream>
+
+unsigned long long Map::default_map_index = 0ull;
 
 Map::Map(const XMLElement& elem) {
     if(!LoadFromXML(elem)) {
@@ -26,8 +34,14 @@ void Map::Update(TimeUtils::FPSeconds deltaSeconds) {
 }
 
 void Map::Render(Renderer& renderer) const {
-    for(auto& layer : _layers) {
+    for(const auto& layer : _layers) {
         layer->Render(renderer);
+    }
+}
+
+void Map::DebugRender(Renderer& renderer) const {
+    for(const auto& layer : _layers) {
+        layer->DebugRender(renderer);
     }
 }
 
@@ -37,7 +51,89 @@ void Map::EndFrame() {
     }
 }
 
+Vector2 Map::GetMaxDimensions() const {
+    Vector2 results{ 1.0f, 1.0f };
+    for(const auto& layer : _layers) {
+        const auto& cur_layer_dimensions = layer->tileDimensions;
+        if(cur_layer_dimensions.x > results.x) {
+            results.x = static_cast<float>(cur_layer_dimensions.x);
+        }
+        if(cur_layer_dimensions.y > results.y) {
+            results.y = static_cast<float>(cur_layer_dimensions.y);
+        }
+    }
+    return results;
+}
+
+Camera2D& Map::GetCamera() const {
+    return _camera;
+}
+
+Material* Map::GetTileMaterial() const {
+    return _tileMaterial;
+}
+
+Layer* Map::GetLayer(std::size_t index) {
+    if(index >= _layers.size()) {
+        return nullptr;
+    }
+    return _layers[index].get();
+}
+
 bool Map::LoadFromXML(const XMLElement& elem) {
-    UNUSED(elem);
-    return false;
+
+    DataUtils::ValidateXmlElement(elem, "map", "tiles,layers,material", "name");
+
+    {
+        std::ostringstream ss;
+        ss << "MAP " << ++default_map_index;
+        std::string default_name = ss.str();
+        _name = DataUtils::ParseXmlAttribute(elem, "name", default_name);
+    }
+
+    if(auto xml_material = elem.FirstChildElement("material")) {
+        DataUtils::ValidateXmlElement(*xml_material, "material", "", "name");
+        auto src = DataUtils::ParseXmlAttribute(*xml_material, "name", std::string{"__invalid"});
+        _tileMaterial = g_theRenderer->GetMaterial(src);
+    }
+
+    if(auto xml_tileset = elem.FirstChildElement("tiles")) {
+        DataUtils::ValidateXmlElement(*xml_tileset, "tiles", "", "src");
+        auto src = DataUtils::ParseXmlAttribute(*xml_tileset, "src", std::string{});
+        if(src.empty()) {
+            ERROR_AND_DIE("Map tiles source is empty.");
+        }
+        tinyxml2::XMLDocument doc;
+        auto xml_result = doc.LoadFile(src.c_str());
+        if(xml_result != tinyxml2::XML_SUCCESS) {
+            std::ostringstream ss;
+            ss << "Map at " << src << " failed to load.";
+            ERROR_AND_DIE(ss.str().c_str());
+        }
+        if(auto xml_root = doc.RootElement()) {
+            DataUtils::ValidateXmlElement(*xml_root, "tileDefinitions", "spritesheet,tileDefinition", "");
+            if(auto xml_spritesheet = xml_root->FirstChildElement("spritesheet")) {
+                if(auto* spritesheet = g_theRenderer->CreateSpriteSheet(*xml_spritesheet)) {
+                    DataUtils::ForEachChildElement(*xml_root, "tileDefinition",
+                        [spritesheet](const XMLElement& elem) {
+                        TileDefinition::CreateTileDefinition(elem, spritesheet);
+                    });
+                }
+            }
+        }
+    }
+
+    if(auto xml_layers = elem.FirstChildElement("layers")) {
+        DataUtils::ValidateXmlElement(*xml_layers, "layers", "layer", "");
+        std::size_t layer_count = DataUtils::GetChildElementCount(*xml_layers, "layer");
+        int layer_index = 0;
+        _layers.reserve(layer_count);
+        DataUtils::ForEachChildElement(*xml_layers, "layer",
+            [this, &layer_index](const XMLElement& xml_layer) {
+            _layers.emplace_back(std::make_unique<Layer>(this, xml_layer));
+            _layers.back()->z_index = layer_index++;            
+        });
+    }
+
+    return true;
 }
--- Roguelike/Code/Game/Map.hpp
+++ Roguelike/Code/Game/Map.hpp
@@ -3,14 +3,20 @@
 #include "Engine/Core/DataUtils.hpp"
 #include "Engine/Core/TimeUtils.hpp"
 
+#include "Engine/Renderer/Camera2D.hpp"
+
+#include <memory>
+
+#include "Game/Layer.hpp"
+
 class Material;
-class Layer;
 class Renderer;
+class TileDefinition;
 
 class Map {
 public:
     Map() = default;
-    Map(const XMLElement& elem);
+    explicit Map(const XMLElement& elem);
     Map(const Map& other) = default;
     Map(Map&& other) = default;
     Map& operator=(const Map& other) = default;
@@ -20,13 +26,20 @@ public:
     void BeginFrame();
     void Update(TimeUtils::FPSeconds deltaSeconds);
     void Render(Renderer& renderer) const;
+    void DebugRender(Renderer& renderer) const;
     void EndFrame();
 
+    Vector2 GetMaxDimensions() const;
+    Camera2D& GetCamera() const;
+    Material* GetTileMaterial() const;
+    Layer* GetLayer(std::size_t index);
 protected:
 private:
     bool LoadFromXML(const XMLElement& elem);
-
-    std::vector<Layer*> _layers{};
-    Material* _tileMaterial = nullptr;
-
+    std::string _name{};
+    std::vector<std::unique_ptr<Layer>> _layers{};
+    Material* _tileMaterial{};
+    mutable Camera2D _camera{};
+    float _camera_speed = 1.0f;
+    static unsigned long long default_map_index;
 };
