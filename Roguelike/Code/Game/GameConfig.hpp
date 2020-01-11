2176f3f5266d8e80a5150668e20259c1c17b9833
cugone <cugone@gmail.com>
2020-01-10 19:12:17 -0600

    Default values non-zero

--- Roguelike/Code/Game/GameConfig.hpp
+++ Roguelike/Code/Game/GameConfig.hpp
@@ -4,9 +4,9 @@ struct GraphicsOptions {
     float WindowHeight = 900.0f;
     float WindowAspectRatio = WindowWidth / WindowHeight;
     float Fov = 70.0f;
-    float MaxShakeAngle = 0.0f;
-    float MaxShakeOffsetHorizontal = 0.0f;
-    float MaxShakeOffsetVertical = 0.0f;
+    float MaxShakeAngle = 45.0f;
+    float MaxShakeOffsetHorizontal = 1.0f;
+    float MaxShakeOffsetVertical = 1.0f;
     float MaxMouseSensitivityX = 0.1f;
     float MaxShakeSensitivityY = 0.1f;
     bool InvertMouseY = false;
