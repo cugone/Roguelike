13b149a29aaa79046ba4d05cdbf7eaceb9bae8f4
cugone <cugone@gmail.com>
2020-01-25 18:58:27 -0600

    give command adds to the stack directly

--- Roguelike/Code/Game/Game.cpp
+++ Roguelike/Code/Game/Game.cpp
@@ -415,10 +415,7 @@ void Game::RegisterCommands() {
                 g_theConsole->ErrorMsg(ss.str());
                 return;
             }
-            if(entity->inventory.HasItem(item_name)) {
-                entity->inventory.AddStack(item_name, item_count);
-                return;
-            }
+            entity->inventory.AddStack(item_name, item_count);
             auto* asActor = dynamic_cast<Actor*>(entity);
             if(!asActor) {
                 g_theConsole->ErrorMsg("Entity is not an actor.");
