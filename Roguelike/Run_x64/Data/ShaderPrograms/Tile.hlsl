53ec836a90965d5e779cd8c7d97d3c8b5df4bd23
cugone <cugone@gmail.com>
2020-11-26 00:22:43 -0600

    Added material/shader for Tile

---
+++ Roguelike/Run_x64/Data/ShaderPrograms/Tile.hlsl
@@ -0,0 +1,55 @@
+
+cbuffer matrix_cb : register(b0) {
+    float4x4 g_MODEL;
+    float4x4 g_VIEW;
+    float4x4 g_PROJECTION;
+};
+
+cbuffer time_cb : register(b1) {
+    float g_GAME_TIME;
+    float g_SYSTEM_TIME;
+    float g_GAME_FRAME_TIME;
+    float g_SYSTEM_FRAME_TIME;
+}
+
+struct vs_in_t {
+    float3 position : POSITION;
+    float4 color : COLOR;
+    float2 uv : UV;
+};
+
+struct ps_in_t {
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+    float2 uv : UV;
+};
+
+SamplerState sSampler : register(s0);
+
+Texture2D<float4> tDiffuse    : register(t0);
+Texture2D<float4> tNormal   : register(t1);
+Texture2D<float4> tDisplacement : register(t2);
+Texture2D<float4> tSpecular : register(t3);
+Texture2D<float4> tOcclusion : register(t4);
+Texture2D<float4> tEmissive : register(t5);
+
+
+ps_in_t VertexFunction(vs_in_t input_vertex) {
+    ps_in_t output;
+
+    float4 local = float4(input_vertex.position, 1.0f);
+    float4 world = mul(local, g_MODEL);
+    float4 view = mul(world, g_VIEW);
+    float4 clip = mul(view, g_PROJECTION);
+
+    output.position = clip;
+    output.color = input_vertex.color;
+    output.uv = input_vertex.uv;
+
+    return output;
+}
+
+float4 PixelFunction(ps_in_t input_pixel) : SV_Target0{
+    float4 albedo = tDiffuse.Sample(sSampler, input_pixel.uv);
+    return albedo * input_pixel.color;
+}
---
+++ Roguelike/Run_x64/Data/Shaders/Tile.shader
@@ -0,0 +1,13 @@
+<shader name="tile">
+    <shaderprogram src="Data/ShaderPrograms/Tile_VS.cso" />
+    <shaderprogram src="Data/ShaderPrograms/Tile_PS.cso" />
+    <raster src="__solid" />
+    <sampler src="__point" />
+    <blends>
+        <blend enable="true">
+            <color src="src_alpha" dest="inv_src_alpha" op="add" />
+        </blend>
+    </blends>
+    <depth enable="false" writable="false" />
+    <stencil enable="false" readable="false" writable="false" />
+</shader>
